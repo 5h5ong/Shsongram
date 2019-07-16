e49c68fc53057e4c843a04ff11dc66bc2a031711
5h5ong <Call5h5ong@gmail.com>
2019-07-16 21:32:39 +0900

    #3.6 Passport JWT part Three

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -3,15 +3,16 @@ import { prisma } from '../../../../generated/prisma-client';
 
 export default {
   Mutation: {
-    requestSecret: async (_, args) => {
+    requestSecret: async (_, args, { request }) => {
+      console.log(request);
       const { email } = args;
       const loginSecret = generateSecret();
       try {
+        throw Error();
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         await sendSecretMail(email, loginSecret);
         return true;
       } catch {
-        console.log(error);
         return false;
       }
     }
