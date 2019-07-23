e072babc5a4b72f6923a2d182df924cf68525279
5h5ong <Call5h5ong@gmail.com>
2019-07-23 22:10:38 +0900

    #3.16 itsMe and amIFollowing Fields part Two

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -5,28 +5,21 @@ export default {
     fullName: parent => {
       return `${parent.firstName} ${parent.lastName}`;
     },
-    amIFollowing: async (parent, _, { request }) => {
+    isFollowing: async (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
 
       console.log(`request user: ${user.id}`);
       console.log(`parent user: ${parent.username}`);
       try {
-        const exsists = await prisma.$exists.user({
-          AND: [{ id: parentId }, { followers_some: { id: user.id } }]
+        return prisma.$exists.user({
+          AND: [{ id: user.id }, { following_some: { id: parentId } }]
         });
-        console.log(exsists);
-        if (exsists) {
-          return true;
-        } else {
-          return false;
-        }
       } catch {
-        console.log(err);
         return false;
       }
     },
-    itsMe: (parent, _, { request }) => {
+    isSelf: (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
       return user.id === parentId;
