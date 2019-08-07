74f103e5d95b49be8670487fa2e81c10badd7271
5h5ong <Call5h5ong@gmail.com>
2019-08-07 13:40:44 +0900

    createAccount return type fix

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,18 +3,14 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = '', lastName = '', bio = '' } = args;
-      try {
-        await prisma.createUser({
-          username,
-          email,
-          firstName,
-          lastName,
-          bio
-        });
-        return true;
-      } catch {
-        return false;
-      }
+      await prisma.createUser({
+        username,
+        email,
+        firstName,
+        lastName,
+        bio
+      });
+      return true;
     }
   }
 };
