eba02eb80d9e760e9eba581ec8ca2c4d6d104907
5h5ong <Call5h5ong@gmail.com>
2019-08-09 16:02:21 +0900

    createAccount fix

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,6 +3,12 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = '', lastName = '', bio = '' } = args;
+      const exists = await prisma.$exists.user({
+        OR: [{ username }, { email }]
+      });
+      if (exists) {
+        throw Error('이 username / Email은 이미 사용되었습니다.');
+      }
       await prisma.createUser({
         username,
         email,
