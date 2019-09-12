2d1bd1aa3f7261f60cc88b747805dc334932559c
5h5ong <Call5h5ong@gmail.com>
2019-09-12 21:01:47 +0900

    #18.7 Preparing for Upload

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { authenticateJwt } from './passport';
 import schema from './schema';
 import './passport';
 import { isAuthenticated } from './middlewares';
+import { uploadController, uploadMiddleware } from './upload';
 
 const PORT = process.env.PORT || 4000;
 
@@ -17,6 +18,7 @@ const server = new GraphQLServer({
 });
 server.express.use(logger('dev'));
 server.express.use(authenticateJwt);
+server.express.post('/api/upload', uploadMiddleware, uploadController);
 
 server.start({ port: PORT }, () =>
   console.log(`✅  Server running on http://localhost:${PORT}`)
