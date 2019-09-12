9c3fdf8bb5fe7bd2b9377c649081c8207840ebae
5h5ong <Call5h5ong@gmail.com>
2019-09-12 21:43:31 +0900

    #18.8 Uploading to Backend

--- src/upload.js
+++ src/upload.js
@@ -2,7 +2,8 @@ import multer from 'multer';
 const upload = multer({ dest: 'uploads/' });
 export const uploadMiddleware = upload.single('file');
 export const uploadController = (req, res) => {
-  const { file } = req;
-  console.log(file);
-  res.end();
+  const {
+    file: { path }
+  } = req;
+  res.json({ path });
 };
