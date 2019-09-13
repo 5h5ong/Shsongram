bdd1cd25c53d48712210428a05786dee34bcf1b0
5h5ong <Call5h5ong@gmail.com>
2019-09-13 10:17:41 +0900

    #18.9 Uploading to S3

--- src/upload.js
+++ src/upload.js
@@ -1,9 +1,31 @@
 import multer from 'multer';
-const upload = multer({ dest: 'uploads/' });
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
+import { createBrotliCompress } from 'zlib';
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.S3_ACCESSKEY,
+  secretAccessKey: process.env.S3_SECRET_ACCESSKEY,
+  region: 'ap-northeast-2'
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    acl: 'public-read',
+    bucket: 'shsongram',
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString());
+    }
+  })
+});
 export const uploadMiddleware = upload.single('file');
 export const uploadController = (req, res) => {
   const {
-    file: { path }
+    file: { location }
   } = req;
-  res.json({ path });
+  res.json({ location });
 };
